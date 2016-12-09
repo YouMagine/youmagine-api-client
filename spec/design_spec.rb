4ed666b5f88a2daf987029113c778497bf2bcda7
Jordy Schreuders <jordy@youmagine.com>
2016-12-09 15:47:26 +0100

    Spec that `Design.all` returns a paginated collection of `Design`s

--- spec/design_spec.rb
+++ spec/design_spec.rb
@@ -1,12 +1,17 @@
-
-
 describe Youmagine::Design do
   context "all" do
-    it "returns designs" do
+    it "returns a list of designs and defaults to the first page" do
       VCR.use_cassette "youmagine_designs_all" do
         designs = Youmagine::Design.all
 
-        byebug
+        expect(designs.first.id).to eq 14712
+        expect(designs.first.name).to eq "Stryfe 180 Motor Cover"
+        expect(designs.first.image).to include(:original, :small, :medium, :large)
+        expect(designs.first.image[:small]).to eq "https://s3-eu-west-1.amazonaws.com/files.youmagine.com/uploads/image/file/155054/small_jamrendering_14712_5219420161209-18447-bsqu24.png"
+        expect(designs.current_page).to eq 1
+      end
+    end
+
       end
     end
   end
